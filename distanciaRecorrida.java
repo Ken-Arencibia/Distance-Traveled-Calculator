ae5ff0eeff2da5943d18b88994ba15f3da0370e1
Marco-Romero <122071151+MazMorr@users.noreply.github.com>
2024-04-26 00:09:38 +0200

    Update distanciaRecorrida.java

--- distanciaRecorrida.java
+++ distanciaRecorrida.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 public class distanciaRecorrida{
 //el tiempo está definido en segundos
-    public static int tiempo(int n1,int n2){
-
+    public static int tiempo(int a){
+        return  a;
     }
 //la aceleracion en metros por segundo cuadrado
     public static int aceleracion(){
-
+        return;
     }
 //Velocidad en metros por segundo
     public static int velocidadInicial(){
-
+        return;
     }
 //Posicón inicial en metros
     public static int posicionInicial(){
+        return;
 
     }
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Hola Mundo");
+        System.out.println("Introduzca el tiempo: ");
+        int time= sc.nextInt();
+        System.out.println(1/2*aceleracion()*(tiempo(time)*tiempo(time))+(velocidadInicial()*tiempo(time)) + posicionInicial());
         sc.close();
     }
 }
